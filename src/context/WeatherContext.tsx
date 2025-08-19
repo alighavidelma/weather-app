de8d5bdf2d4c17a3c71a5b5ef8aaba246f016351
ali ghavidel <alighavidelma@gmail.com>
2025-08-19 22:18:28 +0330

    weather context created

---
+++ src/context/WeatherContext.tsx
@@ -0,0 +1,33 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  type ReactNode,
+} from "react";
+import { type WeatherContextType, type WeatherData } from "../types";
+
+const WeatherContext = createContext<WeatherContextType | undefined>(undefined);
+
+export const WeatherProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  return (
+    <WeatherContext.Provider
+      value={{
+        weather,
+        setWeather,
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export const useWeather = (): WeatherContextType => {
+  const context = useContext(WeatherContext);
+  if (!context) {
+    throw new Error("useWeather must be used within  a provider");
+  }
+  return context;
+};
