d14a317f46b75c09655de8873a38d61df97e5a91
ali ghavidel <alighavidelma@gmail.com>
2025-08-19 22:43:13 +0330

    fetchWeather added into services folder

---
+++ src/services/weatherApi.ts
@@ -0,0 +1,22 @@
+import { type WeatherData } from "../types";
+
+const API_KEY = "4a0c80befc704eb48ca9667affd1b920";
+
+export const fetchWeather = async (city: string): Promise<WeatherData> => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
+
+  const data = await response.json();
+
+  return {
+    city: data.name,
+    temp: data.main.temp,
+    description: data.weather[0].description,
+    humidity: data.main.humidity,
+    wind: data.wind.speed,
+  };
+};
