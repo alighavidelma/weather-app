fe3644ff632a8fb43dcdf27d3d70b21d52c0ed76
ali ghavidel <alighavidelma@gmail.com>
2025-08-23 22:13:17 +0330

    created ToggleThemeButton for dark and light mode

---
+++ src/components/ToggleThemeButton.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+import { useTheme } from "../context/ThemeContext";
+import { Sun, Moon } from "lucide-react";
+
+export default () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <motion.button
+      whileTap={{ scale: 0.9, rotate: 10 }}
+      onClick={toggleTheme}
+      className="p-2 rounded-full shadow-md bg-white dark:bg-gray-800 transition"
+    >
+      {theme === "light" ? (
+        <Moon className="w-6 h-6 text-gray-800" />
+      ) : (
+        <Sun className="w-6 h-6 text-yellow-400" />
+      )}
+    </motion.button>
+  );
+};
