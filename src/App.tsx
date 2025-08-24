1667a6254b43bf96e8a17f8f593110a1d9e98d46
ali ghavidel <alighavidelma@gmail.com>
2025-08-25 00:20:30 +0330

    browserRouter / Routes / Route added into the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import ForecastPage from "./pages/Forecast";
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/forecast" element={<ForecastPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
